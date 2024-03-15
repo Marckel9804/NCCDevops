252415962611b2c390d552baf99616c967dc8bc6
Marckel9804 <ckdgh10421@gmail.com>
2024-03-15 13:11:21 +0900

    before lunch #0315

--- Java/src/main/java/org/Study/Mar/s11/Bingo.java
+++ Java/src/main/java/org/Study/Mar/s11/Bingo.java
@@ -6,29 +6,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Bingo {
+    //현재 빙고판이 3빙고인지 확인하는 함수
     static boolean check(int[][] arr){
         int cnt=0;
+
         for (int i = 0; i <5; i++) {
+            //세로 확인
             if(arr[i][0]==0 && arr[i][1]==0 && arr[i][2]==0 && arr[i][3]==0 && arr[i][4]==0){
                 cnt++;
             }
+            //가로 확인
             if(arr[0][i]==0 && arr[1][i]==0 && arr[2][i]==0 && arr[3][i]==0 && arr[4][i]==0){
                 cnt++;
             }
         }
+        //대각선 확인하기
         if(arr[0][0]==0 && arr[1][1]==0 && arr[2][2]==0 && arr[3][3]==0 && arr[4][4]==0){
             cnt++;
         }
         if(arr[0][4]==0 && arr[1][3]==0 && arr[2][2]==0 && arr[3][1]==0 && arr[4][0]==0){
             cnt++;
         }
-        
+        //3빙고이면 true 아니면 false 리턴
         if(cnt >=3){
             return true;
         } else return false;
     }
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int[][] board = new int[5][5];
@@ -45,20 +49,23 @@ public class Bingo {
                 bingo[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int cnt=0;
+        //3빙고인지 확인하기 위한 변수
+        boolean cnt=false;
         for(int i=0;i<25;i++){
             for (int j = 0; j < 25; j++) {
+                //현재 사회자가 부른 숫자가 보드판의 숫자이면 0으로 바꾸기
                 if(bingo[i/5][i%5]==board[j/5][j%5]){
                     board[j/5][j%5]=0;
+                    //check static함수 실행해서 현재 보드판이 3빙고인지 확인하기
                     if(check(board)){
+                        //현재 사회자가 숫자 부른 횟수 출력하고 종료
                         System.out.println(i+1);
-                        cnt++;
+                        cnt=true;
                         break;
                     }
                 }
             }
-            if(cnt!=0){
+            if(cnt){
                 break;
             }
         }
--- Java/src/main/java/org/Study/Mar/s11/FindSevenTiny.java
+++ Java/src/main/java/org/Study/Mar/s11/FindSevenTiny.java
@@ -8,17 +8,20 @@ import java.util.Arrays;
 public class FindSevenTiny {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //배열 입력
         int[] arr = new int[9];
         int sum =0;
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             sum+=arr[i];
         }
+        //배열 정렬
         Arrays.sort(arr);
 
+        //선택할 두명의 난쟁이 고르기 -> sum -(x+y) = 100
         int x=-1,y=-1;
         for (int i = 0; i < 8; i++) {
-            for (int j = i; j < 9; j++) {
+            for (int j = i+1; j < 9; j++) {
                 if(sum -(arr[i]+arr[j])==100){
                     x=i;
                     y=j;
@@ -29,7 +32,9 @@ public class FindSevenTiny {
                 break;
             }
         }
+        // 배열 한개씩 출력
         for (int i = 0; i < 9; i++) {
+            //arr[x], arr[y]는 출력하지 않고 다음 i로 넘어가기
             if(i==x || i==y){
                 continue;
             }
