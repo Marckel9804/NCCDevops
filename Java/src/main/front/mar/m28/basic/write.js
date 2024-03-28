6bc5bcc44c6c4517c62d7362ee58138569f95a37
Marckel9804 <ckdgh10421@gmail.com>
2024-03-28 13:00:08 +0900

    before lunch!

---
+++ Java/src/main/front/mar/m28/basic/write.js
@@ -0,0 +1,23 @@
+alert('안녕하세요')
+document.write('안녕하세요2')
+console.log('안녕하세요')
+
+document.write('<br>');
+document.write('오늘 목요일');
+document.write('<a href="https://www.naver.com">네이버</a>');
+
+//bool
+document.write(100>20);
+document.write(true);
+
+// prompt
+prompt
+
+//object : js 가 제어 가능한 요소
+document.write(document);
+document.write('<br>');
+
+//undefined : 변수에 값이 할당되지 않을때
+var a;
+document.write (a);
+document.write ('<br');
